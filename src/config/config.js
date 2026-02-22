4aedf3e0bde1d38f21c76353830bd927edeafaa9
Shreesh Badiger <shreeshbadiger12@outlook.com>
2026-02-22 20:22:40 +0530

    fix build issue

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 //import 'dotenv/config';
 const apiKey = import.meta.env.VITE_API_KEY;
-console.log(apiKey);
 const supabaseUrl = "https://vaxdddjzhvwgrgffyjvo.supabase.co";
 const supabaseAnonKey = apiKey;
 
